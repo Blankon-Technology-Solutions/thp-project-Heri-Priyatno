a72f4fc79e483a1729e30e1f34c5936d389f6555
Heri Priyatno <duakilo.net@gmail.com>
2024-04-01 14:44:19 +0700

    feat: Simple windows reload when notified

--- src/thp/settings/channels.py
+++ src/thp/settings/channels.py
@@ -1,14 +1,18 @@
+from .base import env
 ASGI_APPLICATION = 'thp.asgi.application'
+
 # CHANNEL_LAYERS = {
 #     'default': {
 #         'BACKEND': 'channels.layers.InMemoryChannelLayer',
 #     },
 # }
+
+CHANNEL_REDIS = env.tuple('CHANNEL_REDIS', default=("127.0.0.1", 6379))
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [CHANNEL_REDIS],
         },
     },
 }
--- src/todo/models.py
+++ src/todo/models.py
@@ -1,9 +1,17 @@
+import logging
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
 from account.models import User
 
+logger = logging.getLogger()
 
-# Create your models here.
+
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
@@ -13,3 +21,11 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.title
+
+
+@receiver(post_save, sender=Todo)
+def update_todo(sender, instance: Todo, **kwargs):
+    logger.info(f"POST SAVE: {instance.title}")
+    channel_layer = get_channel_layer()
+    todo_group = f"todo_{slugify(instance.user.email)}"
+    async_to_sync(channel_layer.group_send)(todo_group, {"type": "todo.update"})
